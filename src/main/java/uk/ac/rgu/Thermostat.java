9c3680b28e58afe4d0764672e94515eaac2694c9
forson12 <gabrielforson.gh@gmail.com>
2022-09-26 16:09:38 +0100

    moved

---
+++ src/main/java/uk/ac/rgu/Thermostat.java
@@ -0,0 +1,38 @@
+package uk.ac.rgu;
+
+public class Thermostat {
+    public String myTurn;
+    
+
+    // Default Constructor method 
+    public Thermostat(){
+        System.out.println("Input Either (up/down)");
+        myTurn = "not set"; 
+    }
+
+    //Parametric Constructor 
+    public Thermostat(String aMyTurn){
+        myTurn = aMyTurn; 
+    }
+
+    //Getter Mathods 
+    public String getMyTurn(){
+        return myTurn; 
+    }
+
+    //Setters 
+    /*
+    public void setMyTurn(String myTurn){
+        myTurn = aMyTurn;
+    }
+    */
+
+    // Other methods 
+
+    // ToString Method 
+    public String toString(){
+        String output = "";
+        output += "The Thermostat is " + getMyTurn(); 
+        return output; 
+    }
+}
